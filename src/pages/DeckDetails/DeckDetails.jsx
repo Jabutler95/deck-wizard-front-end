b4052e5c23670cc411087faa51e4f6a6ee4730da
joshuabutler <butlerja.nhs@gmail.com>
2024-05-13 12:15:12 -0400

    save progress

---
+++ src/pages/DeckDetails/DeckDetails.jsx
@@ -0,0 +1,28 @@
+// css
+import styles from './DeckDetails.module.css'
+
+// components 
+import DeckCard from '../../components/DeckCard'
+
+const DeckList = (props) => {
+  return (
+    <main className={styles.list}>
+      <h1>DECK LIST</h1>
+
+      {!props.decks.length && <h2>Oops! No decks here!</h2>}
+
+      <ul>
+        {props.decks.map((deck) => (
+          <DeckCard
+          deck={deck}
+          key={deck._id}
+          handleRemoveDeck={props.handleRemoveDeck}
+          />
+        ))}
+      </ul>
+
+    </main>
+  )
+}
+
+export default DeckList
